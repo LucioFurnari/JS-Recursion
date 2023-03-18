1498a360deca23adea4da4c295f47b380725a46f
LucioFurnari <luciofurnari99@gmail.com>
2023-03-18 16:08:17 -0300

    Add method size, head and tail

--- linkedList.js
+++ linkedList.js
@@ -1,10 +1,12 @@
 class LinkedList {
   constructor() { 
     this.Head = null;
+    this.size = 0;
   }
   append(value) {
     if(this.Head === null) {
       this.Head = new Node(value)
+      this.size += 1;
     } else {
       let node = new Node(value)
       let tmp = this.Head
@@ -12,11 +14,26 @@ class LinkedList {
       tmp = tmp.nextNode
       }
       tmp.nextNode = node 
+      this.size += 1;
     }
   }
   prepend(value) {
     if(this.Head === null) this.Head = new Node(value)
     this.Head = new Node(value, this.Head);
+    this.size += 1;
+  }
+  size(){
+    return this.size;
+  }
+  head() {
+    return this.Head;
+  }
+  tail() {
+    let tile = this.Head.nextNode;
+    while (tile.nextNode != null) {
+      tile = tile.nextNode;
+    }
+    return tile;
   }
 }
 
@@ -33,5 +50,9 @@ list.append(2)
 list.append(3)
 list.append(4)
 list.prepend(5)
+list.prepend(6)
+list.size
 
 console.log(list);
+console.log(list.head());
+console.log(list.tail());
